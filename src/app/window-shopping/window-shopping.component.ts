2f7ff8b1b159da6914b4fd39fb6369fb7ee02521
Tharanga Hewavithana <tharanga.hewavithana@cinglevue.com>
2020-06-23 22:25:02 +0530

    Added street view

--- src/app/window-shopping/window-shopping.component.ts
+++ src/app/window-shopping/window-shopping.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { GoogleMap, MapInfoWindow, MapMarker } from '@angular/google-maps';
@@ -18,6 +18,7 @@ export class WindowShoppingComponent implements OnInit, AfterViewInit {
 
   @ViewChild(GoogleMap) googleMap: GoogleMap;
   @ViewChild(MapInfoWindow) infoWindow: MapInfoWindow;
+  @ViewChild('pano') panoElm: ElementRef<HTMLElement>;
 
   center = { lat: 40.7413549, lng: -73.9980244 };
   zoom = 13;
@@ -25,8 +26,6 @@ export class WindowShoppingComponent implements OnInit, AfterViewInit {
 
   locations$: Observable<Location[]>;
   markers$: Observable<google.maps.MarkerOptions[]>;
-
-  selectedLocation: Location;
 
   locations = [
     { title: 'Park Ave Penthouse', location: { lat: 40.7713024, lng: -73.9632393 } },
@@ -39,8 +38,10 @@ export class WindowShoppingComponent implements OnInit, AfterViewInit {
 
   title: string;
   showListings: boolean;
+  private streetViewService: google.maps.StreetViewService;
 
   constructor() {
+    this.streetViewService = new google.maps.StreetViewService();
   }
 
   ngOnInit(): void {
@@ -71,9 +72,28 @@ export class WindowShoppingComponent implements OnInit, AfterViewInit {
 
   openInfoWindow(marker: MapMarker) {
     this.title = marker._marker.getTitle();
+    console.log( this.title)
+
+    const getStreetView = (data, status) => {
+      if (status === google.maps.StreetViewStatus.OK) {
+        const nearStreetViewLocation = data.location.latLng;
+        const heading = google.maps.geometry.spherical.computeHeading(
+          nearStreetViewLocation, marker._marker.getPosition());
+        const panoramaOptions = {
+          position: nearStreetViewLocation,
+          pov: {
+            heading,
+            pitch: 30
+          }
+        };
+        const ll = new google.maps.StreetViewPanorama(this.panoElm.nativeElement, panoramaOptions);
+      }
+    };
+
+    this.streetViewService.getPanoramaByLocation(marker._marker.getPosition(), 50, getStreetView);
+
     this.infoWindow.open(marker);
   }
-
 
   toggleListings() {
     this.showListings = !this.showListings;
